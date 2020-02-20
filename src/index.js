d239419123dec81be1124e79d2f49a2c391372ac
SamarkinaAV <opal-nii@yandex.ru>
2020-02-20 23:54:20 +0400

    init: solution task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (typeof matrix == 'undefined') {
+        return [];
+    }
+    var arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                arr.push(matrix[i][j]);
+            }
+        } else {
+            matrix[i] = matrix[i].reverse();
+            for (let j = 0; j < matrix[i].length; j++) {
+                arr.push(matrix[i][j]);
+            }
+        }
+    }
+    return arr;
 }
